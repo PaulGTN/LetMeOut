0fad8214027ab807dec41d3f8f2a5c1b5dfd827f
Paul.Graton <paulgtn@hotmail.fr>
2019-02-11 15:49:27 +0100

    Tests All ok, 20 ex 0 fail

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :attendances
   has_many :events, through: :attendances
   has_many :events
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :description, presence: true
 end
