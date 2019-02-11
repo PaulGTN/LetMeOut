1aeb9595b0ecb2fe490eb4c014b6505083b9619b
Paul.Graton <paulgtn@hotmail.fr>
2019-02-11 15:33:29 +0100

    Seed done, migrate and model updated

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
   has_many :attendances
   has_many :users, through: :attendances
-  #belongs_to :user(admin?)
+  belongs_to :user
   validates :start_date, presence: true #can't create or modify event in the past
-  validates_datetime :start_date, :after => lambda {Time.now}, :after_message => "Can't be in the past"
-  validates :duration, presence: true, numericality: {only_integer: true}, if: :multiple_of_5: true #min must be multiple of 5 and strictly positive
+  #validates_datetime :start_date, :after => lambda {Time.now}, :after_message => "Can't be in the past"
+  validates :duration, presence: true, numericality: {only_integer: true}#, if: :multiple_of_5: true #min must be multiple of 5 and strictly positive
   validates :title, presence: true, length: {in: 5..140}
   validates :description, presence: true,  length: {in: 20..1000}
   validates :price, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 1000}
